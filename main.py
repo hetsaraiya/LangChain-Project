b6ab1734c86b9c3daba9b55a70f77ce392d28192
het.saraiya <hetsaraiya06@gmail.com>
2024-11-21 17:20:47 +0530

    feat: Implement Streamlit interface for chatbot interaction
    - Added Streamlit to create a web interface for the Ethical Hacking Chatbot.
    - Implemented chat input for user queries.
    - Integrated LLM chain to process user inputs and generate responses.
    - Added title to the Streamlit app.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from app.vectorstore import create_vectorstore
 from app.handle_llm import get_chain
 import os
+import streamlit as s
 
 prompt = "Hello how are you doing today?"
-
 
 if not os.path.exists('faiss_index'):
     create_vectorstore("EN-Ethical Hacking.pdf", "gray-hat-hacking.pdf")
 
-llm_chain = get_chain(query=prompt)
-print(llm_chain)
+s.title("Ethical Hacking Chatbot")
+
+prompt = s.chat_input("You")
+
+if prompt:
+    llm_chain = get_chain(query=prompt)
+    s.write("Bot", llm_chain)
